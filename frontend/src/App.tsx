9cf17b136a7b7c0ddc4624c4ff225643ca7cf027
Kyaw Swar Hein <ksh090358@gmail.com>
2025-10-22 00:01:54 +0700

    feat: Build frontend with React, Vite, and Tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-slate-800 text-white min-h-screen p-8">
+      <h1 className="text-3xl font-bold underline text-cyan-400">
+        Hello NOSBAAN!
+      </h1>
+    </div>
+  )
+}
+export default App
